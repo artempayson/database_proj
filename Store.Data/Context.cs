bb04a132fe96975300613567828c687fe61c57e7
Polina <polina-levashova@yandex.ru>
2016-11-14 12:53:01 +0300

    Added context class

---
+++ Store.Data/Context.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.Data
+{
+    class Context : DbContext
+    {
+        public DbSet<Good> Goods { get; set; }
+
+        public DbSet<Cart> Carts { get; set; }
+
+        public DbSet<CartItem> CartItems { get; set; }
+    }
+}
